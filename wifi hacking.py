d4e70d512da625683f8e9d27c4295894fd40a3f0
Humble-writer <95648883+Ameenu-Jaafar-Umar@users.noreply.github.com>
2025-01-02 23:54:42 -0800

    Add files via upload

---
+++ wifi hacking.py
@@ -0,0 +1,9 @@
+import subprocess
+command1 = 'netsh wlan show profile'
+print(subprocess.run(command1, shell=True, capture_output=True, text=True).stdout)
+print("************************************************************************************")
+SSID = input("""Please choose from the above list which SSID you want the password for:
+************************************************************************************""")
+print("You chose:", SSID)
+command = f'netsh wlan show profile name="{SSID}" key=clear | findstr "Key Content"'
+print(subprocess.run(command, shell=True, capture_output=True, text=True).stdout)
